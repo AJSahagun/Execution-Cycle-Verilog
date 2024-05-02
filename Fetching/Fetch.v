2f9c004c86c9793ac7ce6e58f80935c912e17ecb
AJ <88638495+AJSahagun@users.noreply.github.com>
2024-05-02 17:21:41 +0800

    Fixed Adder Cycle Not Updating
    Removed unnecessary wire and updated test bench

--- Fetching/Fetch.v
+++ Fetching/Fetch.v
@@ -6,13 +6,13 @@ module Fetch(
     input reset, clk,
     output [31:0] instruction, pc_next, pc_current
 );
-    wire [31:0] adder_out, new_pc, pc_address;
+    wire [31:0] new_pc, pc_address;
 
-    Adder PCAdder(pc_address, adder_out); // Add 4 bit
     ProgramCounter PC(clk, reset, new_pc, pc_address); // Program Counter
+    Adder PCAdder(pc_address, new_pc); // Add 4 bit
     InstructionMemory IM(pc_address, clk, instruction); // Get Instruction address
     
     assign pc_current = pc_address;
-    assign pc_next = adder_out;
+    assign pc_next = new_pc;
 
 endmodule
