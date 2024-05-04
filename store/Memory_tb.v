eb895fd10811bdb2791943e50b84251b92f21306
Trisha May Gonzales <126333823+gonzalestrisha@users.noreply.github.com>
2024-05-04 17:44:31 +0800

    Store.v initial commit

--- store/Memory_tb.v
+++ store/Memory_tb.v
@@ -1,7 +1,7 @@
 `include "ALU.v"
-`include "memory.v"
+`include "Memory.v"
 
-module top;
+module memory_tb;
     reg [31:0] a, b;
     reg [2:0] alu_control;
     wire [31:0] result;
@@ -15,6 +15,9 @@ module top;
     memory mem (.address(address), .write_data(write_data), .write_enable(write_enable), .read_data(read_data));
 
     initial begin
+        $dumpfile("Memory_tb.vcd");
+        $dumpvars(0, memory_tb);
+
         a = 32'h00008000; 
         b = 16'h8000; 
         alu_control = 3'b010;
--- store/SignExtend.v
+++ store/SignExtend.v
@@ -1,31 +1,6 @@
-
 module sign_extend (
     input [15:0] in,
     output [31:0] out
 );
     assign out = {{16{in[15]}}, in};
-endmodule
-
-module ALU (
-    input [31:0] a, b,
-    input [2:0] alu_control,
-    output reg [31:0] result,
-    output zero
-);
-    wire [31:0] b_ext;
-
-    sign_extend se (.in(b[15:0]), .out(b_ext));
-
-    always @(*) begin
-        case (alu_control)
-            3'b000: result = a & b_ext;
-            3'b001: result = a | b_ext;
-            3'b010: result = a + b_ext;
-            3'b011: result = a - b_ext;
-            3'b100: result = a < b_ext;
-            default: result = 32'b0;
-        endcase
-    end
-
-    assign zero = (result == 0); 
 endmodule
